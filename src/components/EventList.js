8b90afda3558b4f7c32e83ed564792f8576370bc
Abhishek <abhishekmenon2605@gmail.com>
2022-03-28 16:07:50 +0530

    addedpassingdata

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "./Card";
 
-export default function EventList(props) {
+export default function EventList({option}) {
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-2 justify-center">
       <Card />
       <Card />
       <Card />
       <Card />
-      {props.option}
+      {option}
     </div>
   );
 }
